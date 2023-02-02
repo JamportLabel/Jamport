26cbfacc628c616427fa28acd794b5b82953a58b
Coinmaster <niclas.jung@web.de>
2023-02-02 14:58:43 +0100

    noch mehr angular
    true

--- jamport/src/app/app-routing.module.ts
+++ jamport/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ImpressumComponent } from './pages/impressum/impressum.component';
+import { StoryComponent } from './pages/story/story.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'impressum', component:ImpressumComponent},
+  {path:'story', component:StoryComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- jamport/src/app/app.module.ts
+++ jamport/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StoryComponent } from './pages/story/story.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ImpressumComponent } from './pages/impressum/impressum.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomeComponent,
+    StoryComponent,
+    ContactComponent,
+    ImpressumComponent
   ],
   imports: [
     BrowserModule,
