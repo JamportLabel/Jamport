0f79426dc10b30d06f5495088faac37bcefcbbbf
F Y N X <fi.sch36@web.de>
2023-03-09 22:30:57 +0100

    Alle Pages Fertig

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RaileyComponent } from './pages/artists/railey/railey.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ImpressumComponent } from './pages/impressum/impressum.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
 import { StoryComponent } from './pages/story/story.component';
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path:'contact', component:ContactComponent},
   {path:'impressum', component:ImpressumComponent},
   {path:'story', component:StoryComponent},
+  {path:'privacy', component:PrivacyComponent},
   {path:'artist', children:[
     {path:'railey', component:RaileyComponent},
   ]},
